d300ce1b39f5d0f9571d04b966c68dd43f4a42bd
Gloria R <norimaqidoe@hotmail.com>
2026-04-24 21:51:23 -0600

    delete: CORS handling in Express to avoid duplicate headers

--- app.js
+++ app.js
@@ -28,35 +28,6 @@ mongoose
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.error("Error MongoDB:", err));
 
-const allowedOrigins = [
-  "https://windy.gammavortex.com",
-  "https://www.windy.gammavortex.com",
-  "https://api.windy.gammavortex.com",
-  "http://localhost:3000",
-  "http://localhost:5173",
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS",
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  return next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
